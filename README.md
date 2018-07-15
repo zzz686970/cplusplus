9579c552d6f753501aa53d2c30642bf09ed8540f
zhouzhiz <zzz686970@gmail.com>
2018-07-15 19:14:25 +0800

    pointer function

---
+++ README.md
@@ -0,0 +1,3 @@
+## Long time has passed, time to pick up 
+
+This is a repo for c/c++ exercises.
--- point_ex.cpp
+++ point_ex.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int add_5(int n);
 
+int add_5p(int *p);
+
 int main(){
 	int x;
 	cin >> x;
 	add_5(x);
-	cout << x << endl;
-
+	cout << "x with function:"<< x << endl;
+	add_5p(&x);
+	cout << "x with pointer maniputation:"<< x << endl;
 	// system("pause");
 	return 0;
 }
@@ -18,4 +21,9 @@ int main(){
 int add_5(int n){
 	n = n + 5;
 	return n;
+}
+
+int add_5p(int *p){
+	*p = *p + 5;
+	return *p;
 }
